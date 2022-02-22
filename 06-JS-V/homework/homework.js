bdcfd0a93a3fbed2d917e0f8c5714bb9da672650
moiseslugo <moiseslugo@hotmail.com>
2022-02-22 15:14:02 -0300

    problemas con credenciales

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -27,9 +27,7 @@ function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
-  Constructor.prototype.saludar = function (){
-   return "Hello World!";
-  }
+ 
 }
 
 function agregarStringInvertida() {
